b2d4c3a84c92b2f411fcce1b4ca0b5fb798cdb7b
Vitaly Portnov <vitport@hotmail.com>
2023-06-08 17:22:12 +0300

    fixed Runtovs arrays and merge function

--- data/mergedData.js
+++ data/mergedData.js
@@ -1,6 +1,8 @@
 import arr1 from './questionsPortnov.js';
+import arr2 from './questionsRuntov.js';
+import arr3 from './questionsRuntov2.js';
 
 export default () => {
-  const result = [...arr1];
+  const result = [...arr1, ...arr2, ...arr3];
   return result;
 };
