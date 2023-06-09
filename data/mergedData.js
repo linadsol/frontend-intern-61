3eeb5148b9b359e71f202b616f5beb1c1bf959e0
Vitaly Portnov <vitport@hotmail.com>
2023-06-09 11:35:51 +0300

    fixe Mirco questions, add them to mergerData

--- data/mergedData.js
+++ data/mergedData.js
@@ -1,8 +1,10 @@
 import arr1 from './questionsPortnov.js';
 import arr2 from './questionsRuntov.js';
 import arr3 from './questionsRuntov2.js';
+import arr4 from './questionsMirko.js';
+import arr5 from './questionsMirko2.js';
 
 export default () => {
-  const result = [...arr1, ...arr2, ...arr3];
+  const result = [...arr1, arr2, arr3, arr4, arr5];
   return result;
 };
