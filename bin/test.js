5607dce1b5da4be11de11403ee35a58c418876de
Vitaly <vitport@hotmail.com>
2023-06-17 05:48:43 +0000

    Удалил лишний файл

--- bin/test.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import gameGenerator from '../src/gameGenerator.js';
-import data from '../data/data.js';
-import normalizeDataWithoutFirstLetterInOptions from '../archive/normalizeData.js'
-
-console.log(gameGenerator())
