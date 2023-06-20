a647a7622b2ac0a3cb2e597a4a5b1d17a932ee54
Виктор Рунтов <runtovvs@gmail.com>
2023-06-20 23:54:13 +0200

    доработал smsFriend, теперь функция корректно выдает варианты после применения  функции 50/50

--- src/smsFriend.js
+++ src/smsFriend.js
@@ -1,10 +1,14 @@
 import { getRandomIntInclusive } from './utils.js';
 
 export default (question, roundNumber) => {
+  const normalizeQuestion = question.filter((item) => item !== '    ');
+  const randomAnswer = normalizeQuestion[getRandomIntInclusive(2, normalizeQuestion.length - 1)];
+  const rightAnswer = normalizeQuestion[1];
+
   console.log('Привет, давно не общались :)');
   console.log('Я постарась тебе помочь.');
-  console.log(`Тааак, ${question[0].toLowerCase()}`);
-  const rightAnswer = question[1];
+  console.log(`Тааак, ${normalizeQuestion[0].toLowerCase()}`);
+
   if (roundNumber < 5) {
     console.log(`Хорошо что вопрос такой легкий, я уверен что ответ - ${rightAnswer}`);
   } else if (roundNumber < 10) {
@@ -12,9 +16,9 @@ export default (question, roundNumber) => {
     if (randomNumber > 50) {
       console.log(`Да, вопрос не лёгкий, но я на днях читал об этом. Думаю ответ - ${rightAnswer}`);
     } else {
-      console.log(`Это сложный вопрос. Я не уверен и могу ошибиться, но мне кажется, что ответ - ${question[getRandomIntInclusive(2, 5)]}`);
+      console.log(`Это сложный вопрос. Я не уверен и могу ошибиться, но мне кажется, что ответ - ${randomAnswer}`);
     }
   } else if (roundNumber < 14) {
-    console.log(`Ох.... прости, я не знаю ответа на этот вопрос. Могу только угадать, пусть будет - ${question[getRandomIntInclusive(2, 5)]}`);
+    console.log(`Ох.... прости, я не знаю ответа на этот вопрос. Могу только угадать, пусть будет - ${randomAnswer}`);
   }
 };
