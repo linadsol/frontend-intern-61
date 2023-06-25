4686561c80e36f464437074a016b664c8344f82d
Dmitrii Linkevich <51031656+dm-g8@users.noreply.github.com>
2023-06-25 21:10:30 +0000

    add answersRandomizer.test.js

--- __tests__/answersRandomizer.test.js
+++ __tests__/answersRandomizer.test.js
@@ -6,9 +6,9 @@ const check1 = answersRandomizer(elementOfData);
 const check2 = check1[1].slice(3);
 
 test('Проверка рандомайзера вопросов', () => {
-    expect(Array.isArray(check1)).toBe(true);
-    expect(check1.length).toBe(6);
-    expect(check2).toBe(elementOfData[1]);
+  expect(Array.isArray(check1)).toBe(true);
+  expect(check1.length).toBe(6);
+  expect(check2).toBe(elementOfData[1]);
 
 //    console.log(elementOfData);
 //    console.log(elementOfData[1]);
